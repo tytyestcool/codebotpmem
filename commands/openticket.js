b1f1b4d9c2e73ecf0938ce4ccaa9f0824ad018e5
tytyestcool <107997374+tytyestcool@users.noreply.github.com>
2022-06-26 09:04:52 +0200

    Delete commands directory

--- commands/openticket.js
+++
@@ -1,7 +0,0 @@
-const { Client, Message } = require("discord.js");
-
-module.exports = {
-    run: async (Client, Message, args) => {
-        if(!Message.member.permissions.has('ADMINISTRATOR')) return
-    }
-}
